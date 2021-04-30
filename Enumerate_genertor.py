1fde9eeaf9ea951353534b04cc852ba8efdc0e51
shwethaasrinivasan <shwethasrinivasan8@gmail.com>
2021-04-30 16:40:45 +0530

    Add files via upload

---
+++ Enumerate_genertor.py
@@ -0,0 +1,25 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Fri Apr 30 16:09:44 2021
+
+@author: Barathwaj Kesavan
+"""
+
+lessons = ["Why Python Programming", "Data Types and Operators", "Control Flow", "Functions", "Scripting"]
+
+def my_enumerate(iterable, start=0):
+    count = start
+    for element in iterable:
+        yield count, element
+        count += 1
+
+for i, lesson in my_enumerate(lessons, 1):
+    print("Lesson {}: {}".format(i, lesson))
+    
+
+sq_list = [x**2 for x in range(10)]  # this produces a list of squares
+
+sq_iterator = (x**2 for x in range(10))  # this produces an iterator of squares
+
+print (sq_list)
+print (sq_iterator)
